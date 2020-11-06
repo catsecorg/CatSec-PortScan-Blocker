67233e85b6179dbe482f010210a207b1be14fa01
catsec <github@catsec.org>
2020-11-06 11:29:37 +0100

    Add Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # CatSec-PortScan-Blocker
  tool to detect port scans on a host machine and block IPs
+
+### Python Dependencies
+`pip3 install scapy`
+`pip3 install python_iptables`
+
+### Linux Dependencies
+`sudo apt-get install iptables`
+   
+### Usage
+`sudo python3 catsec_portscan_blocker.py`
+
+#### Usage Options
+```-h, --help            show this help message and exit
+  -H HOSTIP, --hostIP HOSTIP
+                        Specify your host inet address. Will default to the
+                        inet address of default interface.
+  -i INTERFACE, --interface INTERFACE
+                        Specify an interface to sniff for port scans on. Will
+                        default to system default.
+  -c, --clear           Use this flag to clear all blocked IPs before running. 
+```
+ `sudo python3 catsec_portscan_blocker.py -i ens4 -H 10.138.0.27` 
+ 
+ #### Clear out iptable rule chain 
+ `sudo python3 catsec_portscan_blocker.py -c`  
+ or  
+ ``` 
+ sudo iptables -F
+ sudo iptables -X 
+ ```  
+ #### View Blocked IP Addresses
+ `Ctrl + C` while running  
+ or  
+ `sudo iptables -n -L` 
+ 
---
+++ catsec_portscan_blocker.py
@@ -0,0 +1,163 @@
+import argparse
+from collections import OrderedDict
+from datetime import datetime
+import iptc
+import logging
+import os
+from scapy.all import *
+import socket
+import sys
+
+
+
+#This value is how many port connections an IP connects to on the host before it is considered hostile
+SCANTHRESHOLD = 10
+
+logger = logging.getLogger('ScanBlocker')
+class Connection():
+        def __init__(self, pkt):
+                self.src = pkt[IP].src
+                self.dst = pkt[IP].dst
+                self.ports = set()
+                self.time = pkt.time
+                self.timeStamp = datetime.fromtimestamp(pkt.time)
+                self.pkt = pkt
+
+
+def printTime(t):
+        return str(t.strftime('%Y-%m-%d %H:%M:%S.%f'))
+def timeStamp(pkt):
+        return datetime.fromtimestamp(pkt.time)
+
+def getFlags(pkt):
+        F = pkt[TCP].flags
+        return F
+def printFlag(flags,scan):
+    print("Flag: "+ str(flags))
+    if scan:
+        if flags ==0:
+            print("TCP Null Scan")
+        if flags == 1:
+            print("TCP Fin Scan")
+        if flags == 2:
+            print("TCP Syn Scan")
+        if flags == 16:
+            print("TCP Ack Scan")
+        if flags == 18:
+            print("TCP Connect Scan")
+
+
+blockList = set()
+connections = OrderedDict() #a dictionary of connections to the host, maintains insertion order
+maxConnections = 50
+
+
+
+def addConn(key,conn):
+        connections[key] = conn
+        if len(connections) > maxConnections:
+                connections.popitem(False); 
+chain = None
+
+def block(ip):
+    rule = iptc.Rule()
+    rule.protocol = "tcp"
+    rule.src = ip
+    rule.target = rule.create_target("DROP")
+    chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
+    if rule not in chain.rules:
+        chain.insert_rule(rule)
+        print("[!]ADDING RULE TO CHAIN")
+
+def process_packet(pkt):
+        #TCP Connect scan
+        # Connect 18
+        # Syn 2
+        # Fin 1
+        # Ack 16
+        
+        if TCP in pkt:
+                srcIP = pkt[IP].src
+                dstIP = pkt[IP].dst
+                srcPrt = pkt[IP].sport
+                dstPrt = pkt[IP].dport
+                flags = getFlags(pkt)
+
+                if pkt[IP].src in blockList:
+                    rule = iptc.Rule()
+                    rule.src = srcIP
+
+                key = srcIP
+                if key in connections:
+                        connection = connections[key]
+                        connection.timeStamp = timeStamp(pkt)
+                        connection.time = pkt.time
+                        connection.ports.add(dstPrt)
+                        connection.port = pkt[IP].dport
+                if key not in connections:
+                        connection = Connection(pkt)
+                        connection.port = pkt[IP].dport
+                        addConn(key,connection)
+                        print("----------------Adding Connection-----------------")
+                        print("[+] "+ str(datetime.fromtimestamp(connection.time)) +" "+srcIP+":"+str(srcPrt) +" -> "+dstIP+":"+str(dstPrt))
+                        printFlag(flags,False)
+                        print("--------------------------------------------------")
+                if len(connections[key].ports) >= SCANTHRESHOLD:
+                    if connections[key] not in blockList:
+                        blockList.add(connections[key])
+                        print("[!]=============Port scan detected================")
+                        printFlag(flags,True)
+                        
+                        print(str(datetime.fromtimestamp(connections[key].time))+": "+connections[key].src)
+                        block(str(key)) 
+                        block(str(key))
+                        print("--------------------------------------------------")
+                        return
+            
+
+def log(iface=None, hostIP=None):
+        hostIP = str(socket.gethostbyname(socket.gethostname()))
+        filterStr = "ip and dst host "+hostIP
+        sniff(filter= filterStr, prn=process_packet, iface = iface)
+def main():
+
+        #check if user is root/sudo
+        try:
+            if os.geteuid() == 0:
+                parser = argparse.ArgumentParser(description='Detect and block TCP port scans of your host.')
+                parser.add_argument('-H',
+                        '--hostIP',
+                        action='store',
+                        help="Specify your host inet address. Will default to the inet address of default interface.")
+
+                parser.add_argument('-i',
+                       '--interface',
+                       action='store',
+                       help='Specify an interface to sniff for port scans on. Will default to system default.')
+                parser.add_argument('-c',
+                        '--clear',
+                        action='store_true',
+                        help='Use this flag to clear all blocked IPs before running.')
+                args = parser.parse_args()
+                if args.clear:
+                    chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
+                    chain.flush()
+
+                if args.interface and args.hostIP:
+                    log(args.interface,args.hostIP)
+                if args.interface:
+                    log(args.interface)
+                if args.hostIP:
+                    log(None,args.hostIP)
+                else:
+                    log()
+            else:
+                print("[-] Warning: Must run as root.")
+                sys.exit()
+        finally:
+                print("\nIP addresses blocked this session:")
+                print([x.src for x in blockList])
+                sys.exit(0)
+if __name__ == '__main__':
+        main()
+ 
